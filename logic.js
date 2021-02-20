388135b096882999b6a1368e55f9807941bd8e4c
Brooke Cooper <techanalyst65@gmail.com>
2021-02-19 23:04:37 -0500

    Uploading latest...

--- logic.js
+++ logic.js
@@ -1,30 +1,33 @@
+// We create the tile layer that will be the background of our map.
+console.log("Step 1 working");
 // Set start and end dates
-var startDate = '2021-02-11'
-var endDate = '2021-02-18'
+var startDate = '2021-02-11';
+var endDate = '2021-02-18';
 
 // Store our API endpoint inside queryUrl
 // Where are we getting the data?
-var queryUrl = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${startDate}&endtime=${endDate}&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337`;
-
+//var queryUrl = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${startDate}&endtime=${endDate}&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337`;
+var queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 // Perform a GET request to the query URL
 // Fetch the data from the API endpoint
-d3.json(queryUrl).then(data => {
+d3.json(queryUrl), (data) => {  
+  console.log(data.features);
   // Once we get a response, send the data.features object to the createFeatures function
-  
   // Zero in on the features property of your GEOJSON
   earthquakeData = data['features'];
   
-
-  var magnitude = feature.properties.mag  
-  var depth = feature.geometry.coordinates[2]
+  var magnitude = feature.properties.mag; 
+  console.log(magnitude);
+  var depth = feature.geometry.coordinates[2];
+  console.log(depth);
   
   ////////////////////////////////////////////////////////////////////////
-
+  
   // earthquake magnitude correlates to marker size
   // the greater the earthquake magnitude, the larger the marker size
-
+  
   var markerSize = 2000;         // this assumes meters (e.g., 2000 = 2km)
-
+  
   if (magnitude >= 2) {
     markerSize = 6000;
   } else if (magnitude >= 3) {
@@ -42,13 +45,13 @@ d3.json(queryUrl).then(data => {
   } else if (magnitude >= 9) {
     markerSize = 34000;
   }
-
+  
 ////////////////////////////////////////////////////////////////////////
 
 // earthquake depth correlates to color brightness
 // the deeper the earthquake, the darker the color
 
-var markerColor = "#ecffe6"; 
+var markerColor = "#ecffe6";
 
 if (depth >= 5) {
   markerColor = "#c6ffb3";
@@ -70,16 +73,16 @@ if (depth >= 5) {
   markerColor = "#061a00";
 }
 
-  var geojsonMarkerOptions = {
+var geojsonMarkerOptions = {
     radius: markerSize,
     fillColor: markerColor,
     color: "#000",
     weight: 1,
     opacity: 1,
-    fillOpacity: 0.8
-
+    fillOpacity: 0.8}
+    
   // Use L.geoJSON to create a geoJSON layer
-  var earthquakeLayer = L.geoJSON(earthquakeData, {
+  var earthquake = L.geoJSON(earthquakeData, {
     }, {
       pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, geojsonMarkerOptions);
@@ -88,7 +91,6 @@ if (depth >= 5) {
           layer.bindPopup(`<h3>${feature.properties.place}</h3><hr>${new Date(feature.properties.time)}`);
         }
     });
-
     
     // Define streetmap and darkmap layers
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -99,25 +101,25 @@ if (depth >= 5) {
       id: "mapbox/streets-v11",
       accessToken: API_KEY
     });
-
+    
     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
       maxZoom: 18,
       id: "dark-v10",
       accessToken: API_KEY
     });
-
+    
     // Define a baseMaps object to hold our base layers
     var baseMaps = {
       "Street Map": streetmap,
       "Dark Map": darkmap
     };
-
+    
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
       Earthquakes: earthquakeLayer
     };
-
+    
     // Create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("map", {
       center: [
@@ -126,12 +128,11 @@ if (depth >= 5) {
       zoom: 5,
       layers: [streetmap, earthquakeLayer]
     });
-
+    
     // Create a layer control
     // Pass in our baseMaps and overlayMaps
     // Add the layer control to the map
     L.control.layers(baseMaps, overlayMaps, {
       collapsed: false
     }).addTo(myMap);
-
-});
+    
